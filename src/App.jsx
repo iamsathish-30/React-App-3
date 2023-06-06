b17df43cbf34057d23cb3995f8c813e6a71ea8cd
iamsathish-30 <iamsathish3012@gmail.com>
2023-06-06 10:35:02 +0530

    Deleted the subComponents and added data.jsx to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Header from './components/Header.jsx'
+import Meme from './components/Meme.jsx'
 const App = () =>{
     return (
-        <Header/>
+        <>
+            <Header/>
+            <Meme />
+        </>
     );
 }
 
